59ed0d85c1313af8359fabdaf059e0f215cb904f
jenny.kr <jenny.it.kr@gmail.com>
2019-11-19 23:36:46 +0900

    Add set state of movies

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const movies = await axios.get(
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
       "https://yts-proxy.now.sh/list_movies.json"
     );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
