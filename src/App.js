fae5a2e7013e331d3d0d8908eb1ac5eaa57d08b1
jenny.kr <jenny.it.kr@gmail.com>
2019-11-19 23:23:09 +0900

    Add setState of isLoading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ class App extends React.Component {
   state = {
     isLoading: true
   };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
     const { isLoading } = this.state;
     return (
