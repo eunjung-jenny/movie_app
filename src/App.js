7e99ac5409ffcf6a88d14afd8d9fd9cd9223dc09
jenny.kr <jenny.it.kr@gmail.com>
2019-11-19 23:58:19 +0900

    Add html part

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -36,7 +40,9 @@ class App extends React.Component {
                 poster={movie.medium_cover_image}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div className="movie">
+      <img
+        src={poster}
+        alt={title}
+        title={title}
+        width="120px"
+      />
+      <div class="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
