0a9866d28253be7319ff2cc9df66aa56049200f4
jenny.kr <jenny.it.kr@gmail.com>
2020-05-21 13:13:27 +0900

    Updates

---
+++ precache-manifest.6188b2d4d40eda72c5d05b1ad68d1508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f592521e48714669083d602bd7a0c1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d330ac64f13902ead0d4",
+    "url": "/movie_app/static/css/main.45f7a9e7.chunk.css"
+  },
+  {
+    "revision": "e6d12edaf4292912562f",
+    "url": "/movie_app/static/js/2.8768f747.chunk.js"
+  },
+  {
+    "revision": "d330ac64f13902ead0d4",
+    "url": "/movie_app/static/js/main.923c4827.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
